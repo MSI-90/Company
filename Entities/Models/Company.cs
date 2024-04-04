e972c8249d6d876f67ad45855beba6822cfb8488
MSI-90 <s.miler_22@outlook.com>
2024-04-04 13:51:12 +0700

    Создание бибилиотеки классов - Entities, в нем папку Models, здесл будут располагатся модели - сущности, необходимые для сопоставления в таблицы ДБ с помощью ORM.

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Company
+    {
+        [Column("CompanyId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Название компании является обязательным полем.")]
+        [MaxLength(60, ErrorMessage = "Поле Название может содержать не более 60 символов")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Адресс компании является обязателньым полем")]
+        [MaxLength(60, ErrorMessage = "Поле Адресс может содержать не более 60 символов")]
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
