d37facb54cd06cf2b518c908c6e2d4d49346ecfb
MSI-90 <s.miler_22@outlook.com>
2024-04-04 17:29:10 +0700

    1. Создана два конфигурационных класса CompanyConfiguration и EmployeeConfiguration, оба класса реализуют интерфейс IEntityTypeConfiguration<T> 2. В RepositoryContext применены эти конфигурации

---
+++ Company/Migrations/20240404102536_InitialData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Company.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("3d430a70-94ce-4d15-0094-5248280c2ce3"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a7591870"));
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "CompanyId", "Address", "Country", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"), "312 Forest Avenue, BF 923", "USA", "Admin_Solutions LTD" },
+                    { new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"), "583 Wall Dr. Gwynn Oak, MD 21207", "USA", "IT_Solutions LTD" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "CompanyId", "Address", "Country", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("3d430a70-94ce-4d15-0094-5248280c2ce3"), "312 Forest Avenue, BF 923", "USA", "Admin_Solutions LTD" },
+                    { new Guid("c9d4c053-49b6-410c-bc78-2d54a7591870"), "583 Wall Dr. Gwynn Oak, MD 21207", "USA", "IT_Solutions LTD" }
+                });
+        }
+    }
+}
--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
 
 namespace Repository
 {
@@ -9,6 +10,12 @@ namespace Repository
         public DbSet<Employee>? Employees { get; set; }
         public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
         }
     }
 }
