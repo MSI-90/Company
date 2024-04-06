eda467263cf8e559f5c9ebe0c105d9894cbf87fa
MSI-90 <s.miler_22@outlook.com>
2024-04-07 00:37:46 +0700

    Созданы два проекта Service.Contracts и Service оин для контрактов второй для служб - сервисов, созданы интерфейсы и сервисы для работы с репазиторием, боавлен расширяющий меотд как Scoped в ServiceExtensions

--- Company/Extensions/ServiceExtensons/ServiceExtensions.cs
+++ Company/Extensions/ServiceExtensons/ServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Contracts;
 using LoggerService;
 using Repository;
+using Service.Contracts;
+using Service;
 
 namespace CompanyEmployee.Extensions.ServiceExtensons
 {
@@ -21,5 +23,6 @@ namespace CompanyEmployee.Extensions.ServiceExtensons
         public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, LoggerManager>();
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
+        public static void ConfigureServiceManager(this IServiceCollection services) => services.AddScoped<IServiceManager, ServiceManager>();
     }
 }
